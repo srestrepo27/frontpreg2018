c9cb2f0fc898c5e2e85cdc349faa00c4f79e0fe3
s.restrepos1 <s.restrepos1@uniandes.edu.co>
2018-11-21 21:31:53 -0500

    Dte

--- src/app/parcial/parcial-create/parcial-create.component.ts
+++ src/app/parcial/parcial-create/parcial-create.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {ParcialService} from '../parcial.service';
 import {Parcial} from '../parcial';
+import {DatePipe} from '@angular/common';
+//import {ToastrService} from 'ngx-toastr';
 @Component({
   selector: 'app-parcial-create',
   templateUrl: './parcial-create.component.html',
-  styleUrls: ['./parcial-create.component.css']
+  styleUrls: ['./parcial-create.component.css'],
+  providers: [DatePipe]
 })
 export class ParcialCreateComponent implements OnInit {
 
-  constructor(private parcialService: ParcialService) { }
+  constructor(
+    //private toastrService: ToastrService,
+    private parcialService: ParcialService,
+    private dp:DatePipe
+
+    ) { }
   parcial: Parcial;
 
   @Output() cancel= new EventEmitter();
@@ -17,13 +25,18 @@ export class ParcialCreateComponent implements OnInit {
 
   createParcial(): Parcial
   {
+    let dateB: Date = new Date(this.parcial.date.year, this.parcial.date.month - 1, this.parcial.date.day);
+        this.parcial.date = this.dp.transform(dateB, 'yyyy-MM-dd');
     console.log(this.parcial);
 
     this.parcialService.createParcial(this.parcial).subscribe((parcial)=>{this.parcial=parcial; this.create.emit();
-    
+     // this.toastrService.success("The author was created", "Author creation");
     });
     return this.parcial;
   }
+  cancelCreation(): void {
+    this.cancel.emit();
+}
   ngOnInit() {
     this.parcial= new Parcial();
   }
--- src/app/parcial/parcial.ts
+++ src/app/parcial/parcial.ts
@@ -1,5 +1,5 @@
 export class Parcial 
 {
   id: number;
-  des:String;
+  date:any;
 }
